cb1be489b5fcd8890df9104bca53b3bb22a81754
Didier-Lucu <118077001+Didier-Lucu@users.noreply.github.com>
2024-03-22 23:23:58 -0700

    Update config.py

--- Working Code Set/config.py
+++ Working Code Set/config.py
@@ -1,1 +1,13 @@
+# Open API KEY HERE
+open_api_key = ''
 
+
+# Twitter KEYS HERE
+twitter_consumer_key = ''
+twitter_consumer_secret = ''
+twitter_access_token = ''
+twitter_access_token_secret = ''
+
+#SMS KEYS
+EMAIL = ""
+PASSWORD = ""
